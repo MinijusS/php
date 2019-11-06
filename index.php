31427a31e678a2059053a1e7a87d751e9f2c20a5
MinijusS <savickasminijus1@gmail.com>
2019-11-06 16:23:49 +0200

    antras

--- index.php
+++ index.php
@@ -13,8 +13,10 @@ for ($dist = 1; $db > 0; $dist *= 2) {
     if ($db >= 90) {
         if ($my_position < $dist) {
             $situacija = 'keisiu';
+            $img = 'https://i.ytimg.com/vi/C2JZfV61K78/hqdefault.jpg';
         } else {
             $situacija = 'nekeisiu';
+            $img = 'https://n.nordstrommedia.com/id/sr3/9f3cfffc-3b05-4393-b515-e10b05dcacc0.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=1660&h=2546';
         }
     }
 }
@@ -27,5 +29,6 @@ $h3 = "Stovedamas $my_position m. nuo griaustinio, $situacija kelniu";
         <h1><?php print $h1; ?></h1>
         <p><?php print $p; ?></p>
         <h3><?php print $h3; ?></h3>
+        <img src="<?php print $img; ?>">
     </body>
 </html>
